6069f727829780c9b3514539c7fb5f23348c91af
Domingo202 <colbykrober409@gmail.com>
2025-03-11 10:38:54 +0200

    added code

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -9,8 +9,26 @@ package com.mycompany.prjclasses;
  * @author lab_services_student
  */
 public class PrjClasses {
+    
+    
 
     public static void main(String[] args) {
         System.out.println("Hello World! I just created my first gitHub project");
+        
+        newClass nc = new newClass();
+        
+        nc.helloclass();
+        
+        
+        
+        System.out.println(nc.strSecondClassVar);
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
